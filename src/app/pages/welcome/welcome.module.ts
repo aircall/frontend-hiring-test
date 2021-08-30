8234d9ca55f1a6bd198f232716414c6300495757
Tigran Saakyan <tigraqwerty@gmail.com>
2021-08-30 21:02:22 +0200

    ADD: Welcome module and component

---
+++ src/app/pages/welcome/welcome.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WelcomeComponent } from './welcome.component';
+
+
+
+@NgModule({
+  declarations: [
+    WelcomeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class WelcomeModule { }
