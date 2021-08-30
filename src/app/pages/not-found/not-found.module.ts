7fd622533c0fb5a0c8d09d662903c5de5f2e7fbc
Tigran Saakyan <tigraqwerty@gmail.com>
2021-08-30 21:02:00 +0200

    ADD: not found module nad component

---
+++ src/app/pages/not-found/not-found.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NotFoundComponent } from './not-found.component';
+
+
+
+@NgModule({
+  declarations: [
+    NotFoundComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class NotFoundModule { }
