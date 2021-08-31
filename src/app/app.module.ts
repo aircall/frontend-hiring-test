5339690ce87e21e7df33729bd4a5450adf404458
Tigran Saakyan <tigraqwerty@gmail.com>
2021-08-31 09:42:34 +0200

    ADD: Final routing definition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutes } from './app.routing';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, SharedModule.forRoot(), AppRoutes],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/app.routing.ts
@@ -0,0 +1,46 @@
+import { Routes, RouterModule } from '@angular/router';
+import { PrivateWrapperComponent } from './shared/layouts/private-wrapper/private-wrapper.component';
+
+import { PublicWrapperComponent } from './shared/layouts/public-wrapper/public-wrapper.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+  {
+    path: '',
+    component: PublicWrapperComponent,
+    children: [
+      {
+        path: 'welcome',
+        loadChildren: () =>
+          import('./pages/welcome/welcome.module').then((m) => m.WelcomeModule),
+      },
+      {
+        path: 'login',
+        loadChildren: () =>
+          import('./pages/login/login.module').then((m) => m.LoginModule),
+      },
+      {
+        path: '404',
+        loadChildren: () =>
+          import('./pages/not-found/not-found.module').then(
+            (m) => m.NotFoundModule
+          ),
+      },
+    ],
+  },
+  {
+    path: 'private',
+    component: PrivateWrapperComponent,
+    canActivateChild: [],
+    children: [
+      {
+        path: 'calls',
+        loadChildren: () =>
+          import('./pages/calls/calls.module').then((m) => m.CallsModule),
+      },
+    ],
+  },
+  { path: '**', redirectTo: '404' },
+];
+
+export const AppRoutes = RouterModule.forChild(routes);
