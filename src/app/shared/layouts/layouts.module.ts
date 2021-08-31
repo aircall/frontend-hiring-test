89e9530df7dbd0fb3a7b6eb14c80c6eb70fe9954
Tigran Saakyan <tigraqwerty@gmail.com>
2021-08-31 09:56:41 +0200

    ADD: router output tag to layouts components

--- src/app/shared/layouts/layouts.module.ts
+++ src/app/shared/layouts/layouts.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { PublicWrapperComponent } from './public-wrapper/public-wrapper.component';
 import { PrivateWrapperComponent } from './private-wrapper/private-wrapper.component';
 
 @NgModule({
   declarations: [PublicWrapperComponent, PrivateWrapperComponent],
   exports: [PublicWrapperComponent, PrivateWrapperComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
 })
 export class LayoutsModule {}
