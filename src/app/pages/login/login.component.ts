42f1e0f3eaa2d3fe6362fdfc73d1791784e34d46
Tigran Saakyan <tigraqwerty@gmail.com>
2021-08-31 17:09:53 +0200

    ADD: definition of tempalte of login and form functionality

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  host: {
+    class: 'h-100',
+  },
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  public loginForm: FormGroup = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
+  public authData = {
+    username: '',
+    password: '',
+  };
+  public passwordVisible = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  public onSubmit(): void {}
 }
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 import { LoginComponent } from './login.component';
 import { LoginRoutes } from './login.routing';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, LoginRoutes],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    LoginRoutes,
+  ],
 })
 export class LoginModule {}
