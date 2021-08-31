a134b06b351847f3771a86af6c034c8c1f5059c7
Tigran Saakyan <tigraqwerty@gmail.com>
2021-08-31 15:35:29 +0200

    ADD: Angular material lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { RouterModule } from '@angular/router';
 import { AppRoutes } from './app.routing';
 import { AppComponent } from './app.component';
 import { SharedModule } from '@shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule, SharedModule.forRoot(), AppRoutes],
+  imports: [BrowserModule, RouterModule, SharedModule.forRoot(), AppRoutes, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
