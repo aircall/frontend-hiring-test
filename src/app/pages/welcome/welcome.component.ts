fdd786406d56b090ee9bd03cf0de9c1fb0ca5f64
Tigran Saakyan <tigraqwerty@gmail.com>
2021-08-31 15:55:45 +0200

    ADD: add action of start

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { requestProviderType } from '@core/constants/request-provider.constants';
 import { REQUEST_PROVIDER_TYPE } from '@core/models/request-provider.enum';
 
@@ -11,7 +13,7 @@ export class WelcomeComponent implements OnInit, OnDestroy {
   providerTypes: REQUEST_PROVIDER_TYPE[] = requestProviderType;
   selectedProvider: REQUEST_PROVIDER_TYPE = REQUEST_PROVIDER_TYPE.GraphQL;
 
-  constructor() {}
+  constructor(private _router: Router) {}
 
   ngOnInit(): void {
     console.log('TODO: preset privies selected provider');
@@ -20,5 +22,9 @@ export class WelcomeComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     console.log('TODO: save selected provider');
     console.log('TODO: init the provider by type');
+  }
+
+  public start(): void {
+    this._router.navigate(['/login']);
   }
 }
