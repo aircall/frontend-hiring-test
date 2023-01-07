04ab3e1b4f40ecdcefb552c4a08680a4d6536f1a
albcl <a.cerveto@gmail.com>
2023-01-07 19:01:20 +0100

    Some simple tests for date helpers

--- phone-test/src/helpers/dates.spec.ts
+++ phone-test/src/helpers/dates.spec.ts
@@ -1,3 +1,42 @@
-import { formatDate } from './dates';
+import { formatDate, formatDuration } from './dates';
 
-describe('dates helpers', () => {});
+describe('dates helpers', () => {
+  test.each([
+    [0, '00:00'],
+    [600, '10:00'],
+    [36610, '10:10:10']
+  ])('Format call duration (success) - With value %s', (duration, expectedDuration) => {
+    expect(formatDuration(duration)).toEqual(expectedDuration);
+  });
+
+  /**
+   * I would expect values 'null' and '[]' to fail but they don't.
+   * I would consider this a bug on the function.
+   * */
+  test.each(['abc', undefined, {}])('Format call duration (failure) - With value: %s', duration => {
+    expect(() => formatDuration(duration)).toThrowError(/invalid time/i);
+  });
+
+  const successfulSampleData = [
+    ['2022-11-16T13:37:05.822Z', 'Nov 16 - 14:37'],
+    ['2022-11-16', 'Nov 16 - 00:00'],
+    [new Date('January 10, 2002 01:02:03'), 'Jan 10 - 01:02'],
+    [1234567890, 'Jan 15 - 07:56']
+  ];
+  test.each(successfulSampleData)(
+    'Get a valid formatted date (success) - With value: %s',
+    (date, expectedDate) => {
+      expect(() => formatDate(date)).not.toThrowError();
+      expect(formatDate(date)).toEqual(expectedDate);
+    }
+  );
+
+  /**
+   * Null does not fail and returns a valid 'Jan 1 - 01:00'
+   * but in my opinion, I would have expected it to fail instead
+   * */
+  const failSampleData = ['T13:37:05.822Z', 'abc', undefined];
+  test.each(failSampleData)('Get a valid formatted date (failure) - With value: %s', date => {
+    expect(() => formatDate(date)).toThrowError();
+  });
+});
