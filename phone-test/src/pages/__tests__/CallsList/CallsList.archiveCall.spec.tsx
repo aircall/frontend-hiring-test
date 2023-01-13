911411c77d5c8742ff5776191548c85c766775e3
albcl <a.cerveto@gmail.com>
2023-01-13 04:13:57 +0100

    Will archive a call

---
+++ phone-test/src/pages/__tests__/CallsList/CallsList.archiveCall.spec.tsx
@@ -0,0 +1,66 @@
+import { Tractor } from '@aircall/tractor';
+import { MockedProvider } from '@apollo/client/testing';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
+import { ARCHIVE_CALL } from '../../../gql/mutations';
+import { PAGINATED_CALLS } from '../../../gql/queries';
+import { darkTheme } from '../../../style/theme/darkTheme';
+import { CallsListPage } from '../../CallsList';
+import nodesList from './data/nodesList.json';
+
+const requestMocks = [
+  {
+    request: {
+      query: PAGINATED_CALLS,
+      variables: {
+        offset: 0, // Page 1 / Size 5
+        limit: 5
+      }
+    },
+    result: {
+      data: {
+        paginatedCalls: {
+          __typename: 'PaginatedCalls',
+          nodes: nodesList.slice(0, 5), // From 0 to 5 calls
+          totalCount: 100,
+          hasNextPage: true
+        }
+      }
+    }
+  },
+  {
+    request: {
+      query: ARCHIVE_CALL
+    },
+    result: {
+      data: {
+        onUpdateCall: nodesList[1]
+      }
+    }
+  }
+];
+
+const TestingComponent = (
+  <Tractor injectStyle theme={darkTheme}>
+    <MockedProvider mocks={requestMocks} addTypename={false}>
+      <MemoryRouter>
+        <CallsListPage />
+      </MemoryRouter>
+    </MockedProvider>
+  </Tractor>
+);
+
+describe('Archive Call Case', () => {
+  test('Archive a call', async () => {
+    render(TestingComponent);
+
+    const prevClickArchiveOptions = await screen.findAllByText('archive');
+
+    const archiveButton = prevClickArchiveOptions[0];
+    userEvent.click(archiveButton);
+
+    const postClickArchiveOptions = await screen.findAllByText('archive');
+    expect(prevClickArchiveOptions.length).not.toEqual(postClickArchiveOptions.length);
+  });
+});
