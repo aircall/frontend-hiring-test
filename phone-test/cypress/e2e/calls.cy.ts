5eb86d4d3fda5e0f327975ce017c1563a6b9bc53
Oscar C <oscar8x@yahoo.es>
2023-10-23 17:43:24 +0200

    e2e test improvements

--- phone-test/cypress/e2e/calls.cy.ts
+++ phone-test/cypress/e2e/calls.cy.ts
@@ -1,18 +1,23 @@
 /// <reference types="cypress" />
 
 describe('Login and read call', () => {
+  let callId: string;
+
   before(() => {
     cy.fixture('user').then(user => {
       this.user = user;
     });
   });
 
-  it('Login and redirect to calls page', () => {
+  it('Login and redirect to call details', () => {
     cy.visit('/login');
     cy.get('#email').type(this.user.email).should('have.value', this.user.email);
     cy.get('#password').type(this.user.password).should('have.value', this.user.password);
     cy.get('button[type="submit"]').contains('Login').click();
-    cy.wait(4000);
+    cy.wait(2000);
     cy.url().should('include', '/calls');
+    cy.get('div[data-test="call"]').first().click();
+    cy.wait(1000);
+    cy.get('.title').contains('Calls Details');
   });
 });
