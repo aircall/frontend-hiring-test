576952334407d294ac45d4ed4a6b57eead29f3a5
Francisco Rodriguez <fjrodriguez.353@icloud.com>
2023-11-07 01:12:32 +0100

    feat: use mutation refresh token

--- phone-test/src/components/protectedLayout/ProtectedLayout.tsx
+++ phone-test/src/components/protectedLayout/ProtectedLayout.tsx
@@ -2,15 +2,27 @@ import { Button, Flex, Grid, Spacer } from '@aircall/tractor';
 import { Link, Outlet, useNavigate } from 'react-router-dom';
 import logo from '../../assets/logo.png';
 import { useAuth } from '../../hooks/useAuth';
+import { useEffect } from 'react';
 
 export const ProtectedLayout = () => {
-  const { logout, user } = useAuth();
+  const { logout, getNewAccessToken, user } = useAuth();
   const navigate = useNavigate();
 
   const handleLogout = () => {
     logout();
     navigate('/login');
   };
+
+  useEffect(() => {
+    // FIX: This is a temporary solution to refresh the access token
+    getNewAccessToken();
+
+    const nineMinutes = 1000 * 60 * 9;
+    const interval = setInterval(() => {
+      getNewAccessToken();
+    }, nineMinutes);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <Flex flexDirection="column" maxWidth={768} p={4} w="100%">
