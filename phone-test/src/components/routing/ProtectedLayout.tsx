dd41f70510fa8a06923122e5ef7c82dfb490bd47
Daniel Marcano <danielmarcanodev@gmail.com>
2023-11-08 22:42:28 +0100

    feat: show current username within ProtectedLayout through new getMe query

--- phone-test/src/components/routing/ProtectedLayout.tsx
+++ phone-test/src/components/routing/ProtectedLayout.tsx
@@ -1,8 +1,12 @@
 import { Outlet, Link } from 'react-router-dom';
 import { Box, Flex, Spacer, Grid } from '@aircall/tractor';
 import logo from '../../logo.png';
+import { GET_ME } from '../../gql/queries/getMe';
+import { useQuery } from '@apollo/client';
 
 export const ProtectedLayout = () => {
+  const { data } = useQuery(GET_ME);
+
   return (
     <Box minWidth="100vh" p={4}>
       <Flex justifyContent="space-between" alignItems="center">
@@ -10,7 +14,7 @@ export const ProtectedLayout = () => {
           <img src={logo} alt="Aircall" width="32px" height="32px" />
         </Link>
         <Spacer space="m" alignItems="center">
-          <span>{`Welcome {username}!`}</span>
+          <span>{`Welcome ${data?.me?.username}!`}</span>
           <Link to="/login">logout</Link>
         </Spacer>
       </Flex>
