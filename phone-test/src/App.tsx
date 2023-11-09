f7d7a21d2f8a9aa92493f176bc39330c58f6597b
Daniel Marcano <danielmarcanodev@gmail.com>
2023-11-09 19:50:10 +0100

    feat: add authentication logic to ProtectedRoute component, and integrate it within /calls routes

--- phone-test/src/App.tsx
+++ phone-test/src/App.tsx
@@ -5,18 +5,18 @@ import { CallDetailsPage } from './pages/CallDetails';
 import { Tractor } from '@aircall/tractor';
 
 import './App.css';
-import { ProtectedLayout } from './components/routing/ProtectedLayout';
 import { darkTheme } from './style/theme/darkTheme';
 import { RouterProvider } from 'react-router-dom';
 import { GlobalAppStyle } from './style/global';
 import { AuthProvider } from './hooks/useAuth';
 import { ApolloClientProvider } from './ApolloClientProvider';
+import { ProtectedRoute } from './components/routing/ProtectedRoute';
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<AuthProvider />}>
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/calls" element={<ProtectedLayout />}>
+      <Route path="/calls" element={<ProtectedRoute />}>
         <Route path="/calls" element={<CallsListPage />} />
         <Route path="/calls/:callId" element={<CallDetailsPage />} />
       </Route>
--- phone-test/src/components/routing/ProtectedRoute.tsx
+++ phone-test/src/components/routing/ProtectedRoute.tsx
@@ -1,4 +1,13 @@
-export const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
-  // TODO check that the user is authenticated before displaying the route
-  return <>{children}</>;
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../../hooks/useAuth';
+import { ProtectedLayout } from './ProtectedLayout';
+
+export const ProtectedRoute = ({ redirectPath = '/login' }) => {
+  const { status } = useAuth();
+
+  if (status === 'unauthenticated') {
+    return <Navigate to={redirectPath} replace />;
+  }
+
+  return <ProtectedLayout />;
 };
