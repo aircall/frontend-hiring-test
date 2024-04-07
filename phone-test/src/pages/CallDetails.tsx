94e1764572ac18cfd44a806ee49fc6ef95816ae4
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2024-04-07 02:00:22 +0200

    feat(archive-call): Implement archive call in call detail page

--- phone-test/src/pages/CallDetails.tsx
+++ phone-test/src/pages/CallDetails.tsx
@@ -1,16 +1,28 @@
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { useParams } from 'react-router-dom';
 import { GET_CALL_DETAILS } from '../gql/queries/getCallDetails';
-import { Box, Typography } from '@aircall/tractor';
+import { ARCHIVE_CALL } from '../gql/mutations';
+import { Box, Typography, Button, Spacer } from '@aircall/tractor';
 import { formatDate, formatDuration } from '../helpers/dates';
 
 export const CallDetailsPage = () => {
   const { callId } = useParams();
-  const { loading, error, data } = useQuery(GET_CALL_DETAILS, {
+  const { loading, error, data, refetch } = useQuery(GET_CALL_DETAILS, {
     variables: {
       id: callId
     }
   });
+
+  const [archiveCallMutation] = useMutation(ARCHIVE_CALL);
+
+  const handleOnArchiveCall = (callId: string | number): void => {
+    archiveCallMutation({
+      variables: { id: callId },
+      onCompleted: () => {
+        refetch();
+      }
+    });
+  };
 
   if (loading) return <p>Loading call details...</p>;
   if (error) return <p>ERROR</p>;
@@ -29,12 +41,22 @@ export const CallDetailsPage = () => {
         <div>{`Direction: ${call.direction}`}</div>
         <div>{`From: ${call.from}`}</div>
         <div>{`Duration: ${formatDuration(call.duration / 1000)}`}</div>
-        <div>{`Is archived: ${call.is_archived}`}</div>
+        <Spacer>
+          <span>Is archived:</span>
+          <Typography color={`${call.is_archived ? '#FF854C' : '#83B2EE'}`}>
+            {call.is_archived ? 'Archived' : 'No archived'}
+          </Typography>
+        </Spacer>
         <div>{`To: ${call.to}`}</div>
         <div>{`Via: ${call.via}`}</div>
         {call.notes?.map((note: Note, index: number) => {
-          return <div>{`Note ${index + 1}: ${note.content}`}</div>;
+          return <div key={note.id}>{`Note ${index + 1}: ${note.content}`}</div>;
         })}
+        <Spacer my={4}>
+          <Button name="btn-archive-call" size="small" onClick={() => handleOnArchiveCall(call.id)}>
+            {call.is_archived ? `Unarchive call` : `Archive call`}
+          </Button>
+        </Spacer>
       </Box>
     </>
   );
