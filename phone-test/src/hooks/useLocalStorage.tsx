0bc9bf560d898dd24c48fffb428393fd79f313b1
Christian José Torrealba Rondón <torrealba.christian@gmail.com>
2024-04-08 13:40:48 +0200

    Type useLocalStorage hook

--- phone-test/src/hooks/useLocalStorage.tsx
+++ phone-test/src/hooks/useLocalStorage.tsx
@@ -1,6 +1,6 @@
 import { useCallback, useMemo } from 'react';
 
-export const useLocalStorage = (keyName: string, defaultValue: any) => {
+export const useLocalStorage = (keyName: string, defaultValue: unknown) => {
   const storedValue = useMemo(() => {
     const value = window.localStorage.getItem(keyName);
     if (value) {
@@ -11,7 +11,7 @@ export const useLocalStorage = (keyName: string, defaultValue: any) => {
   }, [keyName, defaultValue]);
 
   const setStoredValue = useCallback(
-    (value: any) => {
+    (value: unknown) => {
       if (value) {
         window.localStorage.setItem(keyName, JSON.stringify(value));
       } else {
