3c7bad62b1daffea0faa467ab106a31996eb3078
Julio Costa <juliocosta1@gmail.com>
2024-04-27 20:47:50 +0200

    wip - II

--- phone-test/src/pages/CallsList.tsx
+++ phone-test/src/pages/CallsList.tsx
@@ -1,47 +1,32 @@
 // @ts-nocheck
-import {useState, useEffect} from 'react';
-
+import { useState, useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 import styled from 'styled-components';
 import { PAGINATED_CALLS } from '../gql/queries';
-import {
-  Grid,
-  Icon,
-  Typography,
-  Spacer,
-  Box,
-  DiagonalDownOutlined,
-  DiagonalUpOutlined,
-  Pagination
-} from '@aircall/tractor';
-import {  Form, FormItem, Select } from '@aircall/tractor';
+import { Grid, Icon, Typography, Spacer, Box, DiagonalDownOutlined, DiagonalUpOutlined, Pagination } from '@aircall/tractor';
+import { Form, FormItem, Select } from '@aircall/tractor';
 import { formatDate, formatDuration } from '../helpers/dates';
 import { useNavigate, useSearchParams } from 'react-router-dom';
+import { getValidDate } from '../helpers/dates';
 
 export const typeFilterOptions = [
-  {
-    label: 'Answered',
-    value: 'answered'
-  },
-  {
-    label: 'Missed',
-    value: 'missed'
-  },
-  {
-    label: 'Voicemail',
-    value: 'voicemail'
-  }
+  { label: 'All', value: '' },
+  { label: 'Answered', value: 'answered' },
+  { label: 'Missed', value: 'missed' },
+  { label: 'Voicemail', value: 'voicemail' }
 ];
 
 export const directionFilterOptions = [
-  {
-    label: 'Inbound',
-    value: 'inbound'
-  },
-  {
-    label: 'Outbound',
-    value: 'outbound'
-  }
+  { label: 'All', value: '' },
+  { label: 'Inbound', value: 'inbound' },
+  { label: 'Outbound', value: 'outbound' }
+];
+
+const pageSizeOptions = [
+  { value: 5, label: '5' },
+  { value: 25, label: '25' },
+  { value: 50, label: '50' },
+  { value: 100, label: '100' }
 ];
 
 export const PaginationWrapper = styled.div`
@@ -61,68 +46,21 @@ export const CallsListPage = () => {
   const pageQueryParams = search.get('page');
   const activePage = !!pageQueryParams ? parseInt(pageQueryParams) : 1;
 
-  // const [selectedCallPerPage, setSelectedCallPerPage] = useState(CALLS_PER_PAGE);
-
   const [selectedCallPerPage, setSelectedCallPerPage] = useState(CALLS_PER_PAGE);
   const [callTypeFilter, setCallTypeFilter] = useState('');
-  const [directionFilter, setDirectionFilter] = useState('inbound');
+  const [directionFilter, setDirectionFilter] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
 
-  // need to update schema on server SIDE_OPTIONS. This not being possible need to filter the 
-  // calls locally.
-
-  const { loading, error, data, refetch } = useQuery(PAGINATED_CALLS, {
+  const { loading, error, data } = useQuery(PAGINATED_CALLS, {
     variables: {
-      offset: (activePage - 1) * selectedCallPerPage,
-      limit: selectedCallPerPage,
-      callType: callTypeFilter,
-      direction: directionFilter
+      offset: 0,
+      limit: 200
     }
   });
 
-  const [callTypes, setCallTypes] = useState([]);
-
   useEffect(() => {
-
-  },[])
-
-  // useEffect(() => {
-  //   refetch({
-  //     offset: (activePage - 1) * selectedCallPerPage,
-  //     limit: selectedCallPerPage,
-  //     callType: callTypeFilter,
-  //     direction: directionFilter
-  //   });
-  // }, [activePage, selectedCallPerPage, refetch, callTypeFilter, directionFilter]);
-
-  // const { loading, error, data } = useQuery(PAGINATED_CALLS, {
-  //   variables: {
-  //     offset: (activePage - 1) * CALLS_PER_PAGE,
-  //     limit: CALLS_PER_PAGE
-  //   }
-  //   // onCompleted: () => handleRefreshToken(),
-  // });
-
-  
-  
-
-  const filterCallTypes = (calls: Call[]) => {
-    return calls.filter(call => {
-      // return !direction || call.direction === direction
-      return call.call_type;
-    });
-  };
-
-  const filterCalls = (calls: Call[], callType?: string, direction?: string) => {
-    if (!calls) return [];
-    debugger;
-    return calls.filter(call => {
-      // return !direction || call.direction === direction
-      return (!callType || call.call_type === callType) &&
-             (!direction || call.direction === direction);
-    });
-  };
-
-
+    setCurrentPage(activePage);
+  }, [activePage]);
 
   if (loading) return <p>Loading calls...</p>;
   if (error) return <p>ERROR</p>;
@@ -130,82 +68,65 @@ export const CallsListPage = () => {
 
   const { totalCount, nodes: calls } = data.paginatedCalls;
 
-  // const uniqueCallTypes: string[] = Array.from(new Set(calls.map(call => call.call_type)));
-  
+  console.log({calls})
 
-  // const filteredCalls = filterCalls(data.paginatedCalls.nodes, 'voicemail',);
-  // const filteredCalls = filterCalls(data.paginatedCalls.nodes, 'voicemail', 'inbound');
+  const filteredCalls = filterCalls(calls, callTypeFilter, directionFilter);
 
-  // console.log({filteredCalls})
+  const sortedAndFilteredCallsList = filteredCalls.sort((a: Call, b: Call) => {
+    const dateA = getValidDate(a.created_at).getTime();
+    const dateB = getValidDate(b.created_at).getTime();
+    return dateB - dateA;
+  });
+
+  const paginatedCalls = paginate(sortedAndFilteredCallsList, selectedCallPerPage, currentPage);
 
   const handleCallOnClick = (callId: string) => {
     navigate(`/calls/${callId}`);
   };
 
   const handlePageChange = (page: number) => {
+    setCurrentPage(page);
     navigate(`/calls/?page=${page}`);
   };
 
-  // figure out how to force card width to conform to wrapper
-  // really hate the jump we get while we're loading data
-  // beware of key props
+  // const handleFilterChange = (type: string, value: string) => {
+  //   if (type === 'type') setCallTypeFilter(value);
+  //   else if (type === 'direction') setDirectionFilter(value);
+  // };
 
-  // ToDo: implement filter & hide pagination display if there's no need for it
-
-  // const filteredCallTypes = filterCallTypes(data.paginatedCalls.nodes);
-
-  // console.log({uniqueCallTypes})
+  console.log({selectedCallPerPage, paginatedCalls, sortedAndFilteredCallsList})
 
   return (
     <>
-      <Typography variant="displayM" textAlign="center" py={3}>
-        Calls History
-      </Typography>
+      <Typography variant="displayM" textAlign="center" py={3}>Calls History</Typography>
       <Form>
-      <Spacer
-        fluid={true}
-        space={3}
-        direction="horizontal"
-        justifyContent="stretch"
-        itemsSized="evenly-sized"
-      >
-        <FormItem label="Call Type">
-          <Select
-            takeTriggerWidth={true}
-            placeholder="All"
-            selectionMode="multiple"
-            size="regular"
-            options={typeFilterOptions}
-            onSelectionChange={currentSelectedKeys => {
-              handleFilterChange('type', currentSelectedKeys.join(','));
-            }}
-          />
-        </FormItem>
-        <FormItem label="Call Direction">
-          <Select
-            takeTriggerWidth={true}
-            placeholder="All"
-            size="regular"
-            options={directionFilterOptions}
-            onSelectionChange={currentSelectedKeys =>
-              handleFilterChange('direction', currentSelectedKeys[0] as string)
-            }
-          />
-        </FormItem>
-      </Spacer>
-    </Form>
-      <div style={{ 
-        // background: 'red', 
-        height: '70vh', overflow: 'auto' }}>
+        <Spacer fluid={true} space={3} direction="horizontal" justifyContent="stretch" itemsSized="evenly-sized">
+          <FormItem label="Call Type">
+            <Select
+              takeTriggerWidth={true}
+              placeholder="All"
+              selectionMode="multiple"
+              size="regular"
+              options={typeFilterOptions}
+              onSelectionChange={(currentSelectedKeys) => setCallTypeFilter(currentSelectedKeys)}
+            />
+          </FormItem>
+          <FormItem label="Call Direction">
+            <Select
+              takeTriggerWidth={true}
+              placeholder="All"
+              size="regular"
+              options={directionFilterOptions}
+              onSelectionChange={(currentSelectedKeys) => setDirectionFilter(currentSelectedKeys[0])}
+            />
+          </FormItem>
+        </Spacer>
+      </Form>
+      <div style={{ height: '65vh', overflow: 'auto' }}>
         <Spacer space={3} direction="vertical" fluid>
-          {calls.map((call: Call) => {
+          {paginatedCalls.map((call: Call) => {
             const icon = call.direction === 'inbound' ? DiagonalDownOutlined : DiagonalUpOutlined;
-            const title =
-              call.call_type === 'missed'
-                ? 'Missed call'
-                : call.call_type === 'answered'
-                ? 'Call answered'
-                : 'Voicemail';
+            const title = call.call_type === 'missed' ? 'Missed call' : call.call_type === 'answered' ? 'Call answered' : 'Voicemail';
             const subtitle = call.direction === 'inbound' ? `from ${call.from}` : `to ${call.to}`;
             const duration = formatDuration(call.duration / 1000);
             const date = formatDate(call.created_at);
@@ -213,7 +134,6 @@ export const CallsListPage = () => {
 
             return (
               <Box
-                //  width="1"
                 minWidth="1"
                 key={call.id}
                 bg="black-a30"
@@ -229,46 +149,48 @@ export const CallsListPage = () => {
                   alignItems="center"
                   px={4}
                   py={2}
-                  // background={'blue'}
                 >
-                  <Box>
-                    <Icon component={icon} size={32} />
-                  </Box>
+                  <Box><Icon component={icon} size={32} /></Box>
                   <Box>
                     <Typography variant="body">{title}</Typography>
                     <Typography variant="body2">{subtitle}</Typography>
                   </Box>
                   <Box>
-                    <Typography variant="caption" textAlign="right">
-                      {duration}
-                    </Typography>
+                    <Typography variant="caption" textAlign="right">{duration}</Typography>
                     <Typography variant="caption">{date}</Typography>
                   </Box>
                 </Grid>
-                <Box px={4} py={2}>
-                  <Typography variant="caption">{notes}</Typography>
-                </Box>
+                <Box px={4} py={2}><Typography variant="caption">{notes}</Typography></Box>
               </Box>
             );
           })}
         </Spacer>
       </div>
-
       {totalCount && (
         <PaginationWrapper>
           <Pagination
-            activePage={activePage}
+            activePage={currentPage}
             pageSize={selectedCallPerPage}
+            pageSizeOptions={pageSizeOptions}
             onPageChange={handlePageChange}
             recordsTotalCount={totalCount}
-            onPageSizeChange={(callsPerPage) => {
-              console.log(`output from page size change: ${callsPerPage}`);
-              setSelectedCallPerPage(callsPerPage)
-            }
-            }
+            onPageSizeChange={(callsPerPage) => setSelectedCallPerPage(callsPerPage)}
           />
         </PaginationWrapper>
       )}
     </>
   );
 };
+
+function filterCalls(calls: Call[], callType?: string, direction?: string): Call[] {
+  if (!calls) return [];
+  return calls.filter(call => (
+    (!callType || callType === '' || call.call_type === callType) &&
+    (!direction || direction === '' || call.direction === direction)
+  ));
+}
+
+function paginate(array: any[], pageSize: number, pageNumber: number): any[] {
+  debugger;
+  return array.slice((pageNumber - 1) * pageSize, pageNumber * pageSize);
+}
