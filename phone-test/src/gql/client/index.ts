cb2a9b34139f8bcca587807048aeeed55ef33f20
Julio Costa <juliocosta1@gmail.com>
2024-04-29 18:29:25 +0200

    rewrite

--- phone-test/src/gql/client/index.ts
+++ phone-test/src/gql/client/index.ts
@@ -1,57 +1,20 @@
 import { ApolloClient, createHttpLink, from, fromPromise, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { onError } from '@apollo/client/link/error';
-// import tokenStorage from '../../helpers/tokenStorage';
 import { REFRESH_TOKEN } from '../mutations';
 
-
-type TokenStorage = {
-    getAccessToken: () => string | null;
-    getRefreshToken: () => string | null;
-    setAccessToken: (accessToken: string | null) => void;
-    setRefreshToken: (refreshToken: string | null) => void;
-  };
-  
-  const tokenStorage: TokenStorage = {
-    getAccessToken: () => {
-      const accessToken = localStorage.getItem('access_token');
-      if (!accessToken) return null;
-      return JSON.parse(accessToken);
-    },
-    getRefreshToken: () => {
-      const refreshToken = localStorage.getItem('refresh_token');
-      if (!refreshToken) return null;
-      return JSON.parse(refreshToken);
-    },
-    setAccessToken: (accessToken: string | null) => {
-      localStorage.setItem('access_token', JSON.stringify(accessToken));
-    },
-    setRefreshToken: (refreshToken: string | null) => {
-      localStorage.setItem('refresh_token', JSON.stringify(refreshToken));
-    }
-  };
-  
-  export default tokenStorage;
-  
+const GRAPHQL_ENDPOINT = 'https://frontend-test-api.aircall.dev/graphql';
 
 const httpLink = createHttpLink({
-  uri: 'https://frontend-test-api.aircall.dev/graphql'
+  uri: GRAPHQL_ENDPOINT
 });
 
 let isRefreshingToken: boolean = false;
 
 const getRefreshedToken = async (): Promise<void> => {
-  // Set refresh token as the current access token to be used in the request from authLink
-  const refreshToken = tokenStorage.getRefreshToken();
-  if (refreshToken) {
-    tokenStorage.setAccessToken(refreshToken);
-  }
+  const refreshToken = localStorage.getItem('refresh_token');
 
-  /**
-   * If we are already refreshing the token, return the current promise
-   * to avoid sending multiple requests
-   */
-  if (isRefreshingToken) return;
+  if (!refreshToken || isRefreshingToken) return;
 
   isRefreshingToken = true;
 
@@ -59,19 +22,19 @@ const getRefreshedToken = async (): Promise<void> => {
     const { data } = await client.mutate({
       mutation: REFRESH_TOKEN,
       variables: {
-        refreshToken: refreshToken
+        refreshToken: JSON.parse(refreshToken)
       }
     });
 
-    isRefreshingToken = false;
-
     const { access_token, refresh_token } = data.refreshTokenV2;
 
-    // Update the tokens in local storage
-    tokenStorage.setAccessToken(access_token);
-    tokenStorage.setRefreshToken(refresh_token);
+    localStorage.setItem('access_token', JSON.stringify(access_token));
+    localStorage.setItem('refresh_token', JSON.stringify(refresh_token));
+
   } catch (e) {
     throw new Error('Error refreshing token');
+  } finally {
+    isRefreshingToken = false;
   }
 };
 
@@ -99,9 +62,9 @@ const errorLink = onError(({ graphQLErrors, operation, forward }) => {
 });
 
 const authLink = setContext((_, { headers }) => {
-  const accessToken = tokenStorage.getAccessToken();
+  const currentAccessToken = localStorage.getItem('access_token');
+  const accessToken = currentAccessToken ? JSON.parse(currentAccessToken) : null;
 
-  // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
