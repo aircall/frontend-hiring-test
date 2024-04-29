4ee1993d0fae4fe2209ec3f2213b787f8c5e16b2
Julio Costa <juliocosta1@gmail.com>
2024-04-29 18:36:04 +0200

    rewrite II

--- phone-test/src/App.tsx
+++ phone-test/src/App.tsx
@@ -1,41 +1,18 @@
-import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+import './App.css';
+import { darkTheme } from './style/theme/darkTheme';
+import { GlobalAppStyle } from './style/global';
+
+import { Tractor } from '@aircall/tractor';
+
+import { ApolloProvider } from '@apollo/client';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+
 import { LoginPage } from './pages/Login/Login';
 import { CallsListPage } from './pages/CallsList';
 import { CallDetailsPage } from './pages/CallDetails';
-import { Tractor } from '@aircall/tractor';
-
-import './App.css';
 import { ProtectedLayout } from './components/routing/ProtectedLayout';
-import { darkTheme } from './style/theme/darkTheme';
-import { RouterProvider } from 'react-router-dom';
-import { GlobalAppStyle } from './style/global';
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
 import { AuthProvider } from './hooks/useAuth';
 import { client } from './gql/client';
-
-// const httpLink = createHttpLink({
-//   uri: 'https://frontend-test-api.aircall.dev/graphql'
-// });
-
-// const authLink = setContext((_, { headers }) => {
-//   // get the authentication token from local storage if it exists
-//   const accessToken = localStorage.getItem('access_token');
-//   const parsedToken = accessToken ? JSON.parse(accessToken) : undefined;
-
-//   // return the headers to the context so httpLink can read them
-//   return {
-//     headers: {
-//       ...headers,
-//       authorization: accessToken ? `Bearer ${parsedToken}` : ''
-//     }
-//   };
-// });
-
-// const client = new ApolloClient({
-//   link: authLink.concat(httpLink),
-//   cache: new InMemoryCache()
-// });
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
