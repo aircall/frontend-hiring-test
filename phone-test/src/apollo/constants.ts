fa8c496bcc2bc55a3e38108ba155c37ca12bcf3b
Hayk Martirosyan <mhayko1997@gmail.com>
2024-06-10 17:11:09 +0400

    fix: Fix WS url

--- phone-test/src/apollo/constants.ts
+++ phone-test/src/apollo/constants.ts
@@ -1,3 +1,3 @@
 export const HTTP_LINK = 'https://frontend-test-api.aircall.dev/graphql';
-export const WS_LINK = 'wss://frontend-test-api.aircall.dev/graphql';
+export const WS_LINK = 'wss://frontend-test-api.aircall.dev/websocket';
 export const UNAUTHORIZED_MESSAGE = 'Unauthorized';
