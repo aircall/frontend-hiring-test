a515c31328e524e03a1b538f5b7057319ead2c0a
Goksel Eryigit <goksel@carbonhealth.com>
2024-06-13 19:09:25 +0300

    types

--- phone-test/src/index.tsx
+++ phone-test/src/index.tsx
@@ -7,9 +7,9 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
